612a605eb2dc6d9f144ced3e0d93735db5b5a573
a <102840087+letter-aa@users.noreply.github.com>
2025-04-22 21:46:52 -0400

    Create obfuscated.js

---
+++ obfuscated.js
@@ -0,0 +1,10 @@
+setTimeout(() => <!-- return (return and function) /*
+(() => null + {let: ಠ_ಠ = [] == ![] && 0.?.constructor()}[([][ಠ_ಠ] + [])[+"9" - -![]]][0] / (void - void + /1/<!--0 
+-!+(("abcefgh"?.a?.[+b]?.[("C")][[, , ,]]
+             [ 0,`1
+    `]<!--,`2`]), if 1 then std::cout << "Hello, world?" end 
+    -->
+    ) + ([]+{})[10]?.indexOf + [][[, ,+1 ,, , , ,, ,, , ,, ,  , , , , , ,]]).match()[0]) || +void+[]+-_-+wow?.awef?.()?.[69]).constructor("console.log`Hello, world!`")(), Infinity) * 
+    --> (~!@#$%^&*(){}|:"<>?_+,./-=[]\;') == (0 / true - false)
+
+    null+ +![] + [],["help",,,,,"how do i do thissssssssssssss",,,,[,,,,,]] + []+undefined?.bro
