252e4f0972a350a2f8b32534079c4771e5c8f41d
a <102840087+kingdudely@users.noreply.github.com>
2025-05-01 14:50:38 -0400

    Update require.js

--- require.js
+++ require.js
@@ -1,5 +1,1 @@
-async function require(url){
-    fetch(url)
-    .then(response => response.text())
-    .then(window.eval)
-}
+const require = url => fetch(url).then(response => response.text()).then(window.eval);
