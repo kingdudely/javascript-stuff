9b46c7c835c0a9ef951ca353e686ee1a6927cd0a
a <102840087+kingdudely@users.noreply.github.com>
2025-05-13 10:55:06 -0400

    Update until.js

--- until.js
+++ until.js
@@ -1,8 +1,8 @@
 const until = (condition, timeout = 1) => new Promise(resolve => {
 	const interval = setInterval(() => {
 		if (condition()) {
-        	clearInterval(interval);
-            resolve();
-        }
+	        	clearInterval(interval);
+	            	resolve();
+        	}
 	}, timeout);
 });
