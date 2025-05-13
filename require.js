24bf400cfc0c4129d807488014f2e08c1b1b1119
a <102840087+kingdudely@users.noreply.github.com>
2025-05-13 10:00:08 -0400

    Update require.js

--- require.js
+++ require.js
@@ -1,1 +1,3 @@
 const require = url => fetch(url).then(response => response.text()).then(window.eval);
+// or
+const require = src => document.head.appendChild(document.createElement("script")).src = src
