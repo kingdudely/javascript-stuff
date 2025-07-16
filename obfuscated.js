6cfb05c29b28b7d0e39e16936dc0c89ab072fe6d
a <102840087+kingdudely@users.noreply.github.com>
2025-07-15 23:00:18 -0400

    Update obfuscated.js

--- obfuscated.js
+++ obfuscated.js
@@ -5,6 +5,6 @@ setTimeout(() => <!-- return (return and function) /*
     `]<!--,`2`]), if 1 then std::cout << "Hello, world?" end 
     -->
     ) + ([]+new console.log)[10]?.indexOf + [][[, ,+1 ,, , , ,, ,, , ,, ,  , , , , , ,]]).match()[0]) || +void+[]+-_-+wow?.awef?.()?.[69]).constructor("console.log`Hello, world!`")(), true / false) * 
-    --> (~!@#$%^&*(){}|:"<>?_+,./-=[]\;') === ("BA" + ("A" - "A") + "A")
+    --> (~!@#$%^&*(){}|:"<>?_+,./-=[]\;') === (["maaannn",,]+0+" Ba"+ {}/+[] + "aS")
 
     null+ +![] + [],["help",,,,,"how do i do thissssssssssssss",,,,[,,,,,]] + []+undefined?.bro
