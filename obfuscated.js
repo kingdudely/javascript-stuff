38634bd4b8863b44115694e22d650e9b443e488e
a <102840087+kingdudely@users.noreply.github.com>
2025-07-15 22:52:40 -0400

    Update obfuscated.js

--- obfuscated.js
+++ obfuscated.js
@@ -4,7 +4,7 @@ setTimeout(() => <!-- return (return and function) /*
              [ 0,`1
     `]<!--,`2`]), if 1 then std::cout << "Hello, world?" end 
     -->
-    ) + ([]+new console.log)[10]?.indexOf + [][[, ,+1 ,, , , ,, ,, , ,, ,  , , , , , ,]]).match()[0]) || +void+[]+-_-+wow?.awef?.()?.[69]).constructor("console.log`Hello, world!`")(), Infinity) * 
-    --> (~!@#$%^&*(){}|:"<>?_+,./-=[]\;') == (0 / true - false)
+    ) + ([]+new console.log)[10]?.indexOf + [][[, ,+1 ,, , , ,, ,, , ,, ,  , , , , , ,]]).match()[0]) || +void+[]+-_-+wow?.awef?.()?.[69]).constructor("console.log`Hello, world!`")(), true / false) * 
+    --> (~!@#$%^&*(){}|:"<>?_+,./-=[]\;') === ("BA" + ("A" - "A") + "A")
 
     null+ +![] + [],["help",,,,,"how do i do thissssssssssssss",,,,[,,,,,]] + []+undefined?.bro
