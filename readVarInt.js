ef51f553d2e63da579d1be515f89da9d28d90c6f
a <102840087+kingdudely@users.noreply.github.com>
2026-06-02 11:28:01 -0400

    Create readVarInt.js

---
+++ readVarInt.js
@@ -0,0 +1,31 @@
+function writeVarInt(value, outputBuffer, offset = 0) {
+	do {
+		outputBuffer[offset++] = (value & 127) | ((value > 127) << 7);
+	} while (value >>>= 7);
+  
+	return offset;
+}
+
+function writeVarInt(value, outputBuffer, offset = 0n) { 
+	value = BigInt(value);
+	
+	do {
+		outputBuffer[offset++] = Number((value & 127n) | (BigInt(value > 127n) << 7n));
+	} while (value >>= 7n);
+		
+	return offset;
+}
+
+function writeVarInt(value, outputBuffer, offset = 0) {
+	value = Math.floor(value); 
+	
+	while (value >= 128) {
+		outputBuffer[offset++] = (value % 128) + 128;
+		value = Math.floor(value / 128); 
+	}
+	
+	outputBuffer[offset++] = value;
+	return offset;
+}
+
+
