3965b258573102c41365be2d644b03e8de685352
a <102840087+kingdudely@users.noreply.github.com>
2026-06-03 08:25:20 -0400

    Update writeVarInt.js

--- writeVarInt.js
+++ writeVarInt.js
@@ -9,3 +9,15 @@ function writeVarInt(value, outputBuffer, offset = 0) {
 
 	return offset;
 }
+// or
+function writeVarInt(value, outputBuffer, offset = 0) { 
+	if (!Number.isSafeInteger(value) || value < 0) {
+		throw new RangeError(`Invalid/unsafe unsigned integer: ${value}`);
+	}
+
+	do {
+		outputBuffer[offset++] = (value & 0x7F) | ((value > 0x7F) * 0x80);
+	} while (value = Math.floor(value / 128));  
+
+	return offset;
+}
