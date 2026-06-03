28c446c6e85e92189bf4eb20c8486b0042ccc0a3
a <102840087+kingdudely@users.noreply.github.com>
2026-06-03 08:27:20 -0400

    Refactor writeVarInt function for clarity

--- writeVarInt.js
+++ writeVarInt.js
@@ -4,20 +4,8 @@ function writeVarInt(value, outputBuffer, offset = 0) {
 	}
 
 	do {
-		outputBuffer[offset++] = (value & 0x7F) | (value > 0x7F ? 0x80 : 0);
-	} while ((value = Math.floor(value / 128)) > 0);  
-
-	return offset;
-}
-// or
-function writeVarInt(value, outputBuffer, offset = 0) { 
-	if (!Number.isSafeInteger(value) || value < 0) {
-		throw new RangeError(`Invalid/unsafe unsigned integer: ${value}`);
-	}
-
-	do {
-		outputBuffer[offset++] = (value & 0x7F) | ((value > 0x7F) * 0x80);
-	} while (value = Math.floor(value / 128));  
+		outputBuffer[offset++] = (value & 0x7F) | (value > 0x7F ? 0x80 : 0); // could be ((value > 0x7F) * 0x80)
+	} while ((value = Math.floor(value / 128)) > 0); // could just be value = Math.floor(value / 128)
 
 	return offset;
 }
