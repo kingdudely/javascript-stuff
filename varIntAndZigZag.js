1f02fcb03af1d533404493978dc675e38cc2af7d
a <102840087+kingdudely@users.noreply.github.com>
2026-06-03 10:26:16 -0400

    Update and rename writeVarInt.js to varIntAndZigZag.js

---
+++ varIntAndZigZag.js
@@ -0,0 +1,21 @@
+const encodeZigZag = (x) => Math.abs(x) * 2 - (x < 0);
+
+function writeUnsignedVarInt(value, outputBuffer, offset) { 
+	if (!Number.isSafeInteger(value) || value < 0) {
+		throw new RangeError(`Invalid/unsafe unsigned integer: ${value}`);
+	}
+
+	do {
+		outputBuffer[offset++] = (value & 0x7F) | (value > 0x7F ? 0x80 : 0); // could just be (value > 0x7F) * 0x80
+	} while ((value = Math.floor(value / 128)) > 0); // could just be value = Math.floor(value / 128)
+
+	return offset;
+};
+
+function writeSignedVarInt(value, outputBuffer, offset) {
+	if (!Number.isSafeInteger(value)) {
+		throw new RangeError(`Unsafe signed integer: ${value}`);
+	}
+
+	return writeUnsignedVarInt(encodeZigZag(value), outputBuffer, offset);
+}
